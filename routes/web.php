dd5b3062bd594bc9e48d2853067c5a87d832d4ff
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-07 09:31:55 +0600

    Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,16 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-// Admin Route
+// Admin Group Middleware
 
 Route::middleware(['auth', 'admin'])->group(function () {
+
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-});
+
+     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+});//End Admin Group Middleware
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
 
 
