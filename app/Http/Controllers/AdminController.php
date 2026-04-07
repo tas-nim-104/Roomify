df4ca455e0ee65edabc276186eafb1807442f2a2
SaziaFarahDeba <sazia.cse.20210104020@aust.edu>
2026-04-07 10:05:13 +0600

    Forget Password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,21 @@
-<?php
-
-namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    public function AdminDashboard()
-    {
-        return view('admin.admin_dashboard');
-    }
+@@ -29,6 +30,14 @@ public function AdminLogin(){
+
+    }// End Method 
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
+
+    }// End Method 
+
+
+
 }
