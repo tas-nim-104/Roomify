5b8f5feacc1455a029cf572873ee1367112fa996
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-11 01:28:02 +0600

    Add/Update web routes for admin profile and dashboard

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,14 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'admin'])->group(function () {
 
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
      Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-});
-Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-//End Admin Group Middleware
+
+     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+
+
+});//End Admin Group Middleware
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
