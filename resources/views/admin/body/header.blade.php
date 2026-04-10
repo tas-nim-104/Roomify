0e81e2245dd1b25c456635ed6b5feb09242df194
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-11 01:27:01 +0600

    Update admin header navigation and profile dropdown

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -382,7 +382,7 @@
 													<img src="assets/images/avatars/avatar-8.png" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
-													<h6 class="msg-name">Peter Costanzo <span class="msg-time float-end">6 hrs
+													<h6 class="msg-name">Sazia Farah <span class="msg-time float-end">6 hrs
 												ago</span></h6>
 													<p class="msg-info">It was popularised in the 1960s</p>
 												</div>
@@ -586,15 +586,15 @@
 					</div>
 					<div class="user-box dropdown px-3">
 						<a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<img src="assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
+							<img src="{{asset('backend/assets/images/avatars/avatar-2.png')}}" class="user-img" alt="user avatar">
 							<div class="user-info">
-								<p class="user-name mb-0">Pauline Seitz</p>
-								<p class="designattion mb-0">Web Designer</p>
+								<p class="user-name mb-0">Tasnim Jarin</p>
+								<p class="designattion mb-0">Hotel Manager</p>
 							</div>
 						</a>
 						<ul class="dropdown-menu dropdown-menu-end">
-							<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-user fs-5"></i><span>Profile</span></a>
-							</li>
+
+							<li><a class="dropdown-item d-flex align-items-center" href="{{ route('admin.profile') }}"><i class="bx bx-user fs-5"></i><span>Profile</span></a></li>
 							<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-cog fs-5"></i><span>Settings</span></a>
 							</li>
 							<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-home-circle fs-5"></i><span>Dashboard</span></a>
@@ -606,7 +606,8 @@
 							<li>
 								<div class="dropdown-divider mb-0"></div>
 							</li>
-							<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-log-out-circle"></i><span>Logout</span></a>
+							<li><a class="dropdown-item d-flex align-items-center" href="{{ route('admin.logout') }}">
+								<i class="bx bx-log-out-circle"></i><span>Logout</span></a>
 							</li>
 						</ul>
 					</div>
