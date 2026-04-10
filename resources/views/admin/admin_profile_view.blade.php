9a9ef7c1577f1ac683a666fe878352d19544898b
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-11 01:17:36 +0600

    Update admin profile view with personal info and layout

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,6 +1,5 @@
 @extends('admin.admin_dashboard')
 @section('admin') 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 <div class="page-content">
 				<!--breadcrumb-->
@@ -35,124 +34,114 @@
 							<div class="col-lg-4">
 								<div class="card">
 									<div class="card-body">
-        <div class="d-flex flex-column align-items-center text-center">
-            <img src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
-            <div class="mt-3">
-                <h4>{{ $profileData->name }}</h4>
-                <p class="text-secondary mb-1">{{ $profileData->email }}</p> 
-            
-            </div>
-        </div>
+										<div class="d-flex flex-column align-items-center text-center">
+											<img src="{{asset('backend/assets/images/avatars/avatar-2.png')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+											<div class="mt-3">
+												<h4>Miss Jarin</h4>
+												<p class="text-secondary mb-1">Admin Controller 1</p>
+												<p class="text-muted font-size-sm">Tejgaon Area, Dhaka, Bangladesh</p>
+												
+											</div>
+										</div>
 										<hr class="my-4" />
-        <ul class="list-group list-group-flush">
-            
-            
-                
-            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram me-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                <span class="text-secondary">codervent</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook me-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                <span class="text-secondary">codervent</span>
-            </li>
-        </ul>
+										<ul class="list-group list-group-flush">
+											
+											
+											<li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+												<h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram me-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
+												<span class="text-secondary">codervent</span>
+											</li>
+											<li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+												<h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook me-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
+												<span class="text-secondary">codervent</span>
+											</li>
+										</ul>
 									</div>
 								</div>
 							</div>
-    <div class="col-lg-8">
-        <div class="card">
-
-            <form action="{{ route('admin.profile.store') }}" method="post" enctype="multipart/form-data">
-                @csrf
-
-            <div class="card-body">
-                <div class="row mb-3">
-                    <div class="col-sm-3">
-                        <h6 class="mb-0"> Name</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        <input type="text" name="name" class="form-control" value="{{ $profileData->name }}" />
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-3">
-                        <h6 class="mb-0">Email</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        <input type="email" name="email"  class="form-control" value="{{ $profileData->email }}" />
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-3">
-                        <h6 class="mb-0">Phone</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        <input type="text" name="phone" class="form-control" value="{{ $profileData->phone }}" />
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-sm-3">
-                        <h6 class="mb-0">Address </h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        <input type="text" name="address" class="form-control" value="{{ $profileData->address }}" />
-                    </div>
-                </div>
-               
-                <div class="row mb-3">
-                    <div class="col-sm-3">
-                        <h6 class="mb-0">Photo </h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        <input class="form-control" name="photo" type="file" id="image">
-                    </div>
-                </div>
-
-
-                <div class="row mb-3">
-                    <div class="col-sm-3">
-                        <h6 class="mb-0">  </h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
-                    </div>
-                </div>
-               
-               
-               
-               
-                <div class="row">
-                    <div class="col-sm-3"></div>
-                    <div class="col-sm-9 text-secondary">
-                        <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
-                    </div>
-                </div>
-            </div>
-        </form>
-
-        </div>
-            
-
-
-    </div>
+							<div class="col-lg-8">
+								<div class="card">
+									<div class="card-body">
+										<div class="row mb-3">
+											<div class="col-sm-3">
+												<h6 class="mb-0">Full Name</h6>
+											</div>
+											<div class="col-sm-9 text-secondary">
+												<input type="text" class="form-control" value="Miss Jarin" />
+											</div>
+										</div>
+										<div class="row mb-3">
+											<div class="col-sm-3">
+												<h6 class="mb-0">Email</h6>
+											</div>
+											<div class="col-sm-9 text-secondary">
+												<input type="text" class="form-control" value="xyz@example.com" />
+											</div>
+										</div>
+										<div class="row mb-3">
+											<div class="col-sm-3">
+												<h6 class="mb-0">Phone</h6>
+											</div>
+											<div class="col-sm-9 text-secondary">
+												<input type="text" class="form-control" value="(880) 1234-456789" />
+											</div>
+										</div>
+										<div class="row mb-3">
+											<div class="col-sm-3">
+												<h6 class="mb-0">Mobile</h6>
+											</div>
+											<div class="col-sm-9 text-secondary">
+												<input type="text" class="form-control" value="(880) 1111-222222" />
+											</div>
+										</div>
+										<div class="row mb-3">
+											<div class="col-sm-3">
+												<h6 class="mb-0">Address</h6>
+											</div>
+											<div class="col-sm-9 text-secondary">
+												<input type="text" class="form-control" value="Tejgaon Area, Dhaka, Bangladesh" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm-3"></div>
+											<div class="col-sm-9 text-secondary">
+												<input type="button" class="btn btn-primary px-4" value="Save Changes" />
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-sm-12">
+										<div class="card">
+											<div class="card-body">
+												<h5 class="d-flex align-items-center mb-3">Project Status</h5>
+												<p>Web Design</p>
+												<div class="progress mb-3" style="height: 5px">
+													<div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>
+												<p>Website Markup</p>
+												<div class="progress mb-3" style="height: 5px">
+													<div class="progress-bar bg-danger" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>
+												<p>One Page</p>
+												<div class="progress mb-3" style="height: 5px">
+													<div class="progress-bar bg-success" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>
+												<p>Mobile Template</p>
+												<div class="progress mb-3" style="height: 5px">
+													<div class="progress-bar bg-warning" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>
+												<p>Backend API</p>
+												<div class="progress" style="height: 5px">
+													<div class="progress-bar bg-info" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-
-      <script type="text/javascript">
-
-        $(document).ready(function(){
-            $('#image').change(function(e){
-                var reader = new FileReader();
-                reader.onload = function(e){
-                    $('#showImage').attr('src',e.target.result);
-                }
-                reader.readAsDataURL(e.target.files['0']);
-            });
-        });
-
-        </script>      
-
 @endsection
