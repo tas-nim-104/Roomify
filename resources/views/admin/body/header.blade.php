03b7e120edd5ed20dfbff621010f850cb4778630
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-12 05:54:08 +0600

    Update header partial

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -584,12 +584,18 @@
 							</li>
 						</ul>
 					</div>
-					<div class="user-box dropdown px-3">
-						<a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<img src="{{asset('backend/assets/images/avatars/avatar-2.png')}}" class="user-img" alt="user avatar">
-							<div class="user-info">
-								<p class="user-name mb-0">Tasnim Jarin</p>
-								<p class="designattion mb-0">Hotel Manager</p>
+
+             @php
+                $id = Auth::user()->id;
+                $profileData = App\Models\User::find($id);
+            @endphp
+
+            <div class="user-box dropdown px-3">
+                <a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" class="user-img" alt="user avatar">
+                    <div class="user-info">
+                        <p class="user-name mb-0">{{ $profileData->name }}</p>
+                        <p class="designattion mb-0">{{ $profileData->email }}</p>
 							</div>
 						</a>
 						<ul class="dropdown-menu dropdown-menu-end">
