f824119d78d6f5703b0e3760fefbee311272fffa
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-11 23:03:09 +0600

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::middleware(['auth', 'admin'])->group(function () {
 
      Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+
 
 });//End Admin Group Middleware
 
