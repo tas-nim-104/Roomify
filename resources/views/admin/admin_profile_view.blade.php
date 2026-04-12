579a2aa5fc8fc8fb4f2f0ba1184631c4221d519e
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-12 11:35:06 +0600

    Update admin profile view

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -11,7 +11,7 @@
 							<ol class="breadcrumb mb-0 p-0">
 								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
 								</li>
-								<li class="breadcrumb-item active" aria-current="page">User Profilep</li>
+								<li class="breadcrumb-item active" aria-current="page">User Profile</li>
 							</ol>
 						</nav>
 					</div>
