e1392f5679e38ea311611690629a21031d30ba4b
tas-nim-104 <tasnim.cse.20210104104@aust.edu>
2026-04-12 11:37:28 +0600

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,11 @@ Route::middleware(['auth', 'admin'])->group(function () {
 
      Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
-    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+
+     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+     
+       Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
 
 
 });//End Admin Group Middleware
